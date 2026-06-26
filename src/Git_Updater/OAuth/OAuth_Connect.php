7595c243ec6338ac22bc80e78fd9cf6888c066d5
Andy Fragen <andy@thefragens.com>
2026-06-26 11:49:11 -0700

    auto-remove after 5 failures

--- src/Git_Updater/OAuth/OAuth_Connect.php
+++ src/Git_Updater/OAuth/OAuth_Connect.php
@@ -16,6 +16,7 @@ use Fragen\Git_Updater\Base;
  * Class OAuth_Connect
  *
  * Handles OAuth connect/disconnect/callback for all git providers.
+ * @phpcs:disable WordPress.PHP.DevelopmentFunctions.error_log_error_log
  */
 class OAuth_Connect {
 
@@ -442,7 +443,7 @@ class OAuth_Connect {
 		$options[ $provider . '_refresh_failures' ] = $failures;
 		update_site_option( 'git_updater', $options );
 
-		if ( $failures >= 3 ) {
+		if ( $failures >= 5 ) {
 			$label = isset( self::PROVIDERS[ $provider ]['label'] ) ? self::PROVIDERS[ $provider ]['label'] : ucfirst( $provider );
 			$this->delete_token( $provider );
 
@@ -463,8 +464,6 @@ class OAuth_Connect {
 	 *
 	 * @param string $provider Provider slug.
 	 * @return string|null New access token or null on failure.
-	 *
-	 * @phpcs:disable WordPress.PHP.DevelopmentFunctions.error_log_error_log
 	 */
 	public function refresh_token( string $provider ): ?string {
 		$connector = $this->get_connector_url();
